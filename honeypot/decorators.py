d58b36f2285ce0e687296f8f9581a0db16f3d017
James Turk <james.p.turk@gmail.com>
2009-04-22 14:23:15 -0400

    HONEYPOT_VALUE and HONEYPOT_VERIFIER

--- honeypot/decorators.py
+++ honeypot/decorators.py
@@ -6,11 +6,16 @@ except ImportError:
 from django.conf import settings
 from django.http import HttpResponseBadRequest
 
+def verify_honeypot_value(val):
+    return val == getattr(settings, 'HONEYPOT_VALUE', '')
+
 def check_honeypot(func=None, field_name=None):
+    verifier = getattr(settings, 'HONEYPOT_VERIFIER', verify_honeypot_value)
+
     def inner(request, *args, **kwargs):
         if request.method == 'POST':
             field = field_name or settings.HONEYPOT_FIELD_NAME
-            if field not in request.POST or request.POST[field] != '':
+            if field not in request.POST or not verifier(request.POST[field]):
                 return HttpResponseBadRequest('Honeypot Error')
         return func(request, *args, **kwargs)
     inner = wraps(func)(inner)
--- honeypot/templatetags/honeypot.py
+++ honeypot/templatetags/honeypot.py
@@ -7,7 +7,8 @@ register = template.Library()
 def render_honeypot_field(field_name=None):
     if not field_name:
         field_name = settings.HONEYPOT_FIELD_NAME
+    value = getattr(settings, 'HONEYPOT_VALUE', '')
     return '''<div style="display: none;">
     <label for="%(fieldname)s">leave this field blank to prove your humanity</label>
-    <input type="text" id="%(fieldname)s" name="%(fieldname)s" />
-    </div>''' % {'fieldname': field_name}
+    <input type="text" id="%(fieldname)s" name="%(fieldname)s" value="%(value)s" />
+    </div>''' % {'fieldname': field_name, 'value': value}
