6d880e174ec08deeed2a2e33c07b2005c00222c9
James Turk <james.p.turk@gmail.com>
2009-11-18 14:04:17 -0500

    0.2.1 bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='django-honeypot',
-    version="0.2.0",
+    version="0.2.1",
     package_dir={'honeypot': 'honeypot'},
     packages=['honeypot', 'honeypot.templatetags'],
     description='Django honeypot field utilities',
