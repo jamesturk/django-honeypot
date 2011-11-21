78718f6c8af3ee617f715cd99395b1941ea7a68e
Pierre Drescher <pierre.drescher@gmail.com>
2011-11-21 15:51:33 -0500

    implemented honeypot field for js

--- honeypot/templatetags/honeypot.py
+++ honeypot/templatetags/honeypot.py
@@ -4,7 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 @register.inclusion_tag('honeypot/honeypot_field.html')
-def render_honeypot_field(use_js_field = None, field_name=None):
+def render_honeypot_field(field_name=None):
     """
         Renders honeypot field named field_name (defaults to HONEYPOT_FIELD_NAME).
     """
@@ -14,9 +14,25 @@ def render_honeypot_field(use_js_field = None, field_name=None):
     if callable(value):
         value = value()
     
-    if use_js_field != None:
-        use_js_field = True if use_js_field == 'True' else False
-    else:
-        use_js_field = getattr(settings, 'HONEYPOT_USE_JS_FIELD', False)
+    use_js_field = getattr(settings, 'HONEYPOT_USE_JS_FIELD', False)
+    display_script_tags = True
     
     return {'fieldname': field_name, 'value': value, 'use_js_field': use_js_field}
+
+
+@register.inclusion_tag('honeypot/honeypot_field.html')
+def render_honeypot_field_js(field_name=None):
+    """
+        Renders honeypot field named field_name (defaults to HONEYPOT_FIELD_NAME).
+    """
+    if not field_name:
+        field_name = settings.HONEYPOT_FIELD_NAME
+    value = getattr(settings, 'HONEYPOT_VALUE', '')
+    if callable(value):
+        value = value()
+    
+    use_js_field = getattr(settings, 'HONEYPOT_USE_JS_FIELD', False)
+    display_script_tags = False
+    
+    return {'fieldname': field_name, 'value': value, 'display_script_tags': display_script_tags,
+        'use_js_field': use_js_field}
