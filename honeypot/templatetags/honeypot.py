a9702447740cab3f933691dcd31c0b8df89bb714
Pierre Drescher <pierre.drescher@gmail.com>
2011-11-21 14:24:52 -0500

    Allow specifying whether to use js field as a honeypot

--- honeypot/templatetags/honeypot.py
+++ honeypot/templatetags/honeypot.py
@@ -4,7 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 @register.inclusion_tag('honeypot/honeypot_field.html')
-def render_honeypot_field(field_name=None):
+def render_honeypot_field(use_js_field = None, field_name=None):
     """
         Renders honeypot field named field_name (defaults to HONEYPOT_FIELD_NAME).
     """
@@ -14,6 +14,9 @@ def render_honeypot_field(field_name=None):
     if callable(value):
         value = value()
     
-    use_js_field = getattr(settings, 'HONEYPOT_USE_JS_FIELD', False)
+    if use_js_field != None:
+        use_js_field = True if use_js_field == 'True' else False
+    else:
+        use_js_field = getattr(settings, 'HONEYPOT_USE_JS_FIELD', False)
     
     return {'fieldname': field_name, 'value': value, 'use_js_field': use_js_field}
