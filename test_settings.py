b968f7d97d81654e82e68bb49304859097dddf48
James Turk <james.p.turk@gmail.com>
2016-10-15 22:37:03 -0400

    fix test settings for Django 1.10

--- test_settings.py
+++ test_settings.py
@@ -8,3 +8,23 @@ INSTALLED_APPS = ('honeypot',)
 SECRET_KEY = 'honeyisfrombees'
 
 MIDDLEWARE_CLASSES = ()
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
