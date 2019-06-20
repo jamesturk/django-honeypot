e7208c5921b0c22ba77888452d910ddcdb6ce9ed
nkay08 <niklas.klipphahn@gmail.com>
2019-06-20 19:58:56 +0200

    Create views.py for class-based view mixin

---
+++ honeypot/views.py
@@ -0,0 +1,10 @@
+class HoneypotMixin(object):
+    honeypot_fieldname = None
+
+    def post(self, request, *args, **kwargs):
+        from django.conf import settings
+        honeypot_fieldname = self.honeypot_fieldname or getattr(settings, 'HONEYPOT_FIELD_NAME', 'family_name')
+        response = verify_honeypot_value(request, honeypot_fieldname)
+        if response:
+            return response
+        return super(HoneypotMixin, self).post(request, *args, **kwargs)
