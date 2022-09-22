2bd50f4bd47c7f2d9103a924b0dccd178e550f3a
James Turk <dev@jamesturk.net>
2022-09-21 19:22:10 -0500

    update black

--- honeypot/tests.py
+++ honeypot/tests.py
@@ -32,13 +32,13 @@ class HoneypotTestCase(TestCase):
 
 class VerifyHoneypotValue(HoneypotTestCase):
     def test_no_call_on_get(self):
-        """ test that verify_honeypot_value is not called when request.method == GET """
+        """test that verify_honeypot_value is not called when request.method == GET"""
         request = _get_GET_request()
         resp = verify_honeypot_value(request, None)
         self.assertEquals(resp, None)
 
     def test_verifier_false(self):
-        """ test that verify_honeypot_value fails when HONEYPOT_VERIFIER returns False """
+        """test that verify_honeypot_value fails when HONEYPOT_VERIFIER returns False"""
         request = _get_POST_request()
         request.POST[settings.HONEYPOT_FIELD_NAME] = ""
         settings.HONEYPOT_VERIFIER = lambda x: False
@@ -53,14 +53,14 @@ class VerifyHoneypotValue(HoneypotTestCase):
         self.assertEquals(resp.__class__, HttpResponseBadRequest)
 
     def test_field_blank(self):
-        """ test that verify_honeypot_value succeeds when HONEYPOT_VALUE is blank """
+        """test that verify_honeypot_value succeeds when HONEYPOT_VALUE is blank"""
         request = _get_POST_request()
         request.POST[settings.HONEYPOT_FIELD_NAME] = ""
         resp = verify_honeypot_value(request, None)
         self.assertEquals(resp, None)
 
     def test_honeypot_value_string(self):
-        """ test that verify_honeypot_value succeeds when HONEYPOT_VALUE is a string """
+        """test that verify_honeypot_value succeeds when HONEYPOT_VALUE is a string"""
         request = _get_POST_request()
         settings.HONEYPOT_VALUE = "(test string)"
         request.POST[settings.HONEYPOT_FIELD_NAME] = settings.HONEYPOT_VALUE
@@ -68,7 +68,7 @@ class VerifyHoneypotValue(HoneypotTestCase):
         self.assertEquals(resp, None)
 
     def test_honeypot_value_callable(self):
-        """ test that verify_honeypot_value succeeds when HONEYPOT_VALUE is a callable """
+        """test that verify_honeypot_value succeeds when HONEYPOT_VALUE is a callable"""
         request = _get_POST_request()
         settings.HONEYPOT_VALUE = lambda: "(test string)"
         request.POST[settings.HONEYPOT_FIELD_NAME] = settings.HONEYPOT_VALUE()
@@ -78,21 +78,21 @@ class VerifyHoneypotValue(HoneypotTestCase):
 
 class CheckHoneypotDecorator(HoneypotTestCase):
     def test_default_decorator(self):
-        """ test that @check_honeypot works and defaults to HONEYPOT_FIELD_NAME """
+        """test that @check_honeypot works and defaults to HONEYPOT_FIELD_NAME"""
         new_view_func = check_honeypot(view_func)
         request = _get_POST_request()
         resp = new_view_func(request)
         self.assertEquals(resp.__class__, HttpResponseBadRequest)
 
     def test_decorator_argument(self):
-        """ test that check_honeypot(view, 'fieldname') works """
+        """test that check_honeypot(view, 'fieldname') works"""
         new_view_func = check_honeypot(view_func, "fieldname")
         request = _get_POST_request()
         resp = new_view_func(request)
         self.assertEquals(resp.__class__, HttpResponseBadRequest)
 
     def test_decorator_py24_syntax(self):
-        """ test that @check_honeypot syntax works """
+        """test that @check_honeypot syntax works"""
 
         @check_honeypot("field")
         def new_view_func(request):
@@ -112,12 +112,12 @@ class RenderHoneypotField(HoneypotTestCase):
         self.assertEquals(rendered, correct)
 
     def test_default_templatetag(self):
-        """ test that {% render_honeypot_field %} works and defaults to HONEYPOT_FIELD_NAME """
+        """test that {% render_honeypot_field %} works and defaults to HONEYPOT_FIELD_NAME"""
         template = Template("{% load honeypot %}{% render_honeypot_field %}")
         self._assert_rendered_field(template, settings.HONEYPOT_FIELD_NAME, "")
 
     def test_templatetag_honeypot_value(self):
-        """ test that {% render_honeypot_field %} uses settings.HONEYPOT_VALUE """
+        """test that {% render_honeypot_field %} uses settings.HONEYPOT_VALUE"""
         template = Template("{% load honeypot %}{% render_honeypot_field %}")
         settings.HONEYPOT_VALUE = "(leave blank)"
         self._assert_rendered_field(
@@ -125,7 +125,7 @@ class RenderHoneypotField(HoneypotTestCase):
         )
 
     def test_templatetag_argument(self):
-        """ test that {% render_honeypot_field 'fieldname' %} works """
+        """test that {% render_honeypot_field 'fieldname' %} works"""
         template = Template(
             '{% load honeypot %}{% render_honeypot_field "fieldname" %}'
         )
@@ -137,7 +137,7 @@ class HoneypotMiddleware(HoneypotTestCase):
     _response_body = '<form method="POST"></form>'
 
     def test_view_middleware_invalid(self):
-        """ don't call view when HONEYPOT_VERIFIER returns False """
+        """don't call view when HONEYPOT_VERIFIER returns False"""
         request = _get_POST_request()
         retval = HoneypotViewMiddleware(lambda request: None).process_view(
             request, view_func, (), {}
@@ -145,7 +145,7 @@ class HoneypotMiddleware(HoneypotTestCase):
         self.assertEquals(retval.__class__, HttpResponseBadRequest)
 
     def test_view_middleware_valid(self):
-        """ call view when HONEYPOT_VERIFIER returns True """
+        """call view when HONEYPOT_VERIFIER returns True"""
         request = _get_POST_request()
         request.POST[settings.HONEYPOT_FIELD_NAME] = ""
         retval = HoneypotViewMiddleware(lambda request: None).process_view(
@@ -154,7 +154,7 @@ class HoneypotMiddleware(HoneypotTestCase):
         self.assertEquals(retval, None)
 
     def test_response_middleware_rewrite(self):
-        """ ensure POST forms are rewritten """
+        """ensure POST forms are rewritten"""
         request = _get_POST_request()
         request.POST[settings.HONEYPOT_FIELD_NAME] = ""
         response = HttpResponse(self._response_body)
@@ -163,7 +163,7 @@ class HoneypotMiddleware(HoneypotTestCase):
         self.assertContains(response, 'name="%s"' % settings.HONEYPOT_FIELD_NAME)
 
     def test_response_middleware_contenttype_exclusion(self):
-        """ ensure POST forms are not rewritten for non-html content types """
+        """ensure POST forms are not rewritten for non-html content types"""
         request = _get_POST_request()
         request.POST[settings.HONEYPOT_FIELD_NAME] = ""
         response = HttpResponse(self._response_body, content_type="text/javascript")
@@ -171,7 +171,7 @@ class HoneypotMiddleware(HoneypotTestCase):
         self.assertContains(response, self._response_body)
 
     def test_response_middleware_unicode(self):
-        """ ensure that POST form rewriting works with unicode templates """
+        """ensure that POST form rewriting works with unicode templates"""
         request = _get_GET_request()
         unicode_body = u"\u2603" + self._response_body  # add unicode snowman
         response = HttpResponse(unicode_body)
@@ -180,7 +180,7 @@ class HoneypotMiddleware(HoneypotTestCase):
         self.assertContains(response, 'name="%s"' % settings.HONEYPOT_FIELD_NAME)
 
     def test_exempt_view(self):
-        """ call view no matter what if view is exempt """
+        """call view no matter what if view is exempt"""
         request = _get_POST_request()
         exempt_view_func = honeypot_exempt(view_func)
         assert exempt_view_func.honeypot_exempt is True
